071f34250c0da7376910506e1beb16a8d788024e
Chirag Khurana <khuranachirag95@gmail.com>
2021-02-11 04:04:55 +0530

    Fix minor issues
    - Fixed permissions on some files
    - Fixed DB access path in source

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, g
 # from generator import gen
 import sqlite3
+import os
 
 
 # FLASK app
@@ -8,13 +9,16 @@ app = Flask(__name__)
 
 
 # DATABASE Utility Functions
-DATABASE = 'db/marks.db'
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+DATABASE = os.path.join(PROJECT_ROOT, 'db', 'marks.db')
+
 
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
+
 
 @app.teardown_appcontext
 def close_connection(exception):
